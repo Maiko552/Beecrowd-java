e994134e063e284b7381621fd338603715e53e42
Maikon Sposito <mknsposito@gmail.com>
2025-08-22 13:56:34 -0300

    feat: desafio NotasEMoedas

--- src/main/java/desafio/Cedulas.java
+++ src/main/java/desafio/Cedulas.java
@@ -1,6 +1,10 @@
 package desafio;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.util.List;
@@ -23,20 +27,28 @@ public class Cedulas {
         symbols.setDecimalSeparator(',');
         DecimalFormat formatador = new DecimalFormat("###,##0.00", symbols);
 
+        System.out.println(valorEmReais);
+
         for (BigDecimal nota : notas) {
             BigDecimal[] resultado = valorEmReais.divideAndRemainder(nota);
             BigDecimal quantidade = resultado[0];
             BigDecimal resto = resultado[1];
+
             if (quantidade.compareTo(BigDecimal.ZERO) > 0) {
+                String notaFormatada = formatador.format(nota);
+                System.out.println(quantidade.intValue() + " nota(s) de R$ " + notaFormatada);
+            }else{
                 String notaFormatada = formatador.format(nota);
                 System.out.println(quantidade.intValue() + " nota(s) de R$ " + notaFormatada);
             }
 
-            valorEmReais = resto;
+            valorEmReais = resto.setScale(2, RoundingMode.DOWN);
         }
     }
 
-    public static void main(String[] args) {
-        Cedulas.RestoDoDinheiro(BigDecimal.valueOf(768));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BigDecimal valor = new BigDecimal(br.readLine());
+        RestoDoDinheiro(valor);
     }
 }
